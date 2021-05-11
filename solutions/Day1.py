ad760d8377b54ff010ecaad5954ce41a0f3954d7
Lucas Xavier <lucasxaviert875@gmail.com>
2021-05-11 09:46:34 -0300

    Day1

--- solutions/Day1.py
+++ solutions/Day1.py
@@ -1,16 +1,9 @@
-def imprime(s1):
-    s2=s1.split(" ")
+N=int(input())
+for i in range(N):
+    temp=input()
+    s2=temp.split(" ")
     s2=sorted(s2,key=len,reverse=True)
-    for temp in s2:
-        print(temp, end=" ")
-    print("")
-
-N=int(input(""))
-lista=[]
-for i in range(0,N):
-    temp=input("")
-    lista.append(temp)
-
-for i in lista:
-    imprime(i)
-print("")
+    nova=""
+    for j in s2:
+        nova+=j+" "
+    print(nova[:-1])
